808cf6995bff20f6e60d58314c0275b80d21dcf5
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2023-10-24 12:28:20 -0300

    att media

--- EXERCICIOS/EX026/mq005/script.js
+++ EXERCICIOS/EX026/mq005/script.js
@@ -11,3 +11,13 @@ function clickmenu(){
     }
     //O tipo de chama vai importar muito, porque estamos chamando a função pelo 'onlclick', ou seja, toda vez que clicarmo no objeto com o id itens, a função ira acontecer
 }
+function mudartamanho(){
+    if (window.innerWidth >= 768){
+        itens.style.display = 'block'
+    }
+    else
+    {
+        itens.style.display = 'none'
+    }
+    
+}
